22e33dd56c89703d22241ea976c7e875164e4240
liutao <2238299830@qq.com>
2020-03-07 21:35:09 +0800

    这个是一个空项目

---
+++ src/main/java/com/smile/DruidApp.java
@@ -0,0 +1,13 @@
+package com.smile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidApp.class, args);
+    }
+
+}
